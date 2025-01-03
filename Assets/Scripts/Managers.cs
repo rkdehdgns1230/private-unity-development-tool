2536505bd703e1ac3f2b4277b60a8b11d23d5f0f
dongHun <actgong@gmail.com>
2025-01-04 01:07:01 +0900

    feature - singleton pattern으로 Managers 객체 생성

---
+++ Assets/Scripts/Managers.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    public static Managers Instance
+    {
+        get
+        {
+            Init();
+            return _instance;
+        }
+    }
+
+    private static Managers _instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    static void Init()
+    {
+        if(_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers") ?? new GameObject("@Managers");
+            _instance = go.GetComponent<Managers>() ?? go.AddComponent<Managers>();
+            DontDestroyOnLoad(go);
+        }
+    }
+}
